08f4812cf0b9dd18a189d71e61089783803c91cb
BalloonsPop <94821399+BalloonsPop@users.noreply.github.com>
2022-08-31 17:24:38 -0600

    Password Generator

---
+++ index.js
@@ -0,0 +1,23 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passOneEl = document.getElementById("passOne")
+let passTwoEl = document.getElementById("passTwo")
+let password = []
+
+generatePasswords.addEventListener("click", function() {
+    passOne.textContent = ""
+    passTwo.textContent = ""
+    passwordGen()
+})
+
+function passwordGen(){
+    for(let i=0; i < 15; i++){
+    let randomChar = Math.floor(Math.random() * characters.length)
+    passOne.textContent += characters[randomChar]
+    }
+    for(let i=0; i < 15; i++){
+    let randomChar = Math.floor(Math.random() * characters.length)
+    passTwo.textContent += characters[randomChar]
+    }
+}
